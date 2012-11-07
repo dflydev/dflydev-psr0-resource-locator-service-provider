2c68701e51666f1e4c38102fa98e23e3a7ffccf3
Beau Simensen <beau@dflydev.com>
2012-11-06 20:46:21 -0800

    Missed a dep.

--- tests/massage-composer-for-testing.php
+++ tests/massage-composer-for-testing.php
@@ -1,5 +1,6 @@
 <?php
 
 $data = json_decode(file_get_contents(__DIR__.'/../composer.json'), true);
+unset($data['require']['dflydev/psr0-resource-locator']);
 unset($data['require']['dflydev/psr0-resource-locator-implementation-service-provider']);
 file_put_contents(__DIR__.'/composer.json', json_encode($data));
